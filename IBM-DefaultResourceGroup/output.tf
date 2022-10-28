d4f19ce8447dfb2d71d896b642d1b150e3fe1de0
Stephen Strutt <steve_strutt@uk.ibm.com>
2022-10-28 15:06:36 +0100

    Update output.tf

--- IBM-DefaultResourceGroup/output.tf
+++ IBM-DefaultResourceGroup/output.tf
@@ -1,9 +1,9 @@
 output "resource_group_id" {
   description = "The ID of the resource group"
-  value       = data.ibm_resource_group.default.resource_group_id
+  value       = data.ibm_resource_group.default[0].id
 }
 output "resource_group_name" {
   description = "The Name of the resource group"
-  value       = data.ibm_resource_group.default.name
+  value       = data.ibm_resource_group.default[0].name
 }
 
